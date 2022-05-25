84c903915abfaad3eada64cd8cdc00bf60675a01
bapunn <kokosunsun1004@Gmail.com>
2022-05-25 09:58:09 +0900

    기본 파일 저장

---
+++ main/java/com/sparta/week3/domain/MemoRequestDto.java
@@ -0,0 +1,11 @@
+package com.sparta.week3.domain;
+
+import lombok.Getter;
+
+@Getter
+public class MemoRequestDto {
+    private String username;
+    private String contents;
+    private String password;
+    private String title;
+}
